5306466158bb13528bdf1b4da1d4e9073c5d3bf6
Anky <anky_au@hotmail.com>
2017-07-02 21:32:38 +1000

    feat: Configure libraries

--- app/src/main/java/me/anky/connectid/MainActivity.java
+++ app/src/main/java/me/anky/connectid/MainActivity.java
@@ -1,7 +1,9 @@
 package me.anky.connectid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.stetho.Stetho;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Stetho.initializeWithDefaults(this);
     }
 }
