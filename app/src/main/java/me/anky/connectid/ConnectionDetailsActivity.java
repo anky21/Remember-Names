c8ff5e39314acacc7df3b07eb3953938f9ea249d
Anky <anky_au@hotmail.com>
2017-07-06 14:41:35 +1000

    feat: Change portrait photo by taking or picking a photo

--- app/src/main/java/me/anky/connectid/ConnectionDetailsActivity.java
+++ app/src/main/java/me/anky/connectid/ConnectionDetailsActivity.java
@@ -3,14 +3,28 @@ package me.anky.connectid;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ConnectionDetailsActivity extends AppCompatActivity {
+    private final static String TAG = ConnectionDetailsActivity.class.getSimpleName();
+
+    @BindView(R.id.portrait_iv)
+    ImageView mPortraitIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +49,22 @@ public class ConnectionDetailsActivity extends AppCompatActivity {
         PickerFragment newFragment = new PickerFragment();
         newFragment.show(fm, "dialog");
         ft.commit();
+    }
+
+    public void changePhoto (Object object){
+        Bitmap bitmap;
+        try {
+            if (object instanceof Uri) {
+                Uri imageUri = (Uri) object;
+                Picasso.with(this).load(imageUri).into(mPortraitIv);
+                InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                bitmap = BitmapFactory.decodeStream(imageStream);
+            } else {
+                bitmap = (Bitmap)object;
+                mPortraitIv.setImageBitmap(bitmap);
+            }
+        } catch (Exception e){
+            Log.e(TAG, "error in changing photo");
+        }
     }
 }
--- app/src/main/java/me/anky/connectid/PickerFragment.java
+++ app/src/main/java/me/anky/connectid/PickerFragment.java
@@ -1,13 +1,21 @@
 package me.anky.connectid;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.view.Window;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -15,7 +23,15 @@ import butterknife.OnClick;
  * A simple {@link Fragment} subclass.
  */
 public class PickerFragment extends android.app.DialogFragment {
+    private static final String TAG = "PICKER_FRAGMENT";
+    private static final int PICK_PHOTO = 100;
+    private static final int TAKE_PHOTO = 101;
 
+    @BindView(R.id.pickImage_tv)
+    TextView mPickImageTv;
+
+    @BindView(R.id.takeImage_tv)
+    TextView mTakeImageTv;
 
     public PickerFragment() {
         // Required empty public constructor
@@ -27,16 +43,47 @@ public class PickerFragment extends android.app.DialogFragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_picker, container, false);
         ButterKnife.bind(this, view);
+
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         return view;
     }
 
     @OnClick(R.id.takeImage_tv)
     public void takeImage(){
-        Toast.makeText(getActivity(), "take a photo", Toast.LENGTH_SHORT).show();
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, TAKE_PHOTO);
+        }
     }
 
     @OnClick(R.id.pickImage_tv)
     public void pickImage(){
-        Toast.makeText(getActivity(), "pick a photo", Toast.LENGTH_SHORT).show();
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, PICK_PHOTO);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case PICK_PHOTO:
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.d(TAG, "Picked a photo.");
+                    Uri selectedImage = data.getData();
+                    ((ConnectionDetailsActivity)getActivity()).changePhoto(selectedImage);
+                    getDialog().dismiss();
+                }
+                break;
+            case TAKE_PHOTO:
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.d(TAG, "Took a photo.");
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    ((ConnectionDetailsActivity)getActivity()).changePhoto(imageBitmap);
+                    getDialog().dismiss();
+                }
+        }
     }
 }
