ab599c8c9aa6ad754219c60a89e005cda7bfc8da
bonoj <johnlbono@gmail.com>
2017-07-11 18:05:20 -0500

    feat: Add UI handling of empty database

--- app/src/main/java/me/anky/connectid/connections/ConnectionsActivity.java
+++ app/src/main/java/me/anky/connectid/connections/ConnectionsActivity.java
@@ -36,8 +36,8 @@ public class ConnectionsActivity extends AppCompatActivity implements
     @BindView(R.id.fab)
     FloatingActionButton fab;
 
-    @BindView(R.id.debug_list_tv)
-    TextView debug_tv;
+    @BindView(R.id.empty_list_tv)
+    TextView emptyListTv;
 
     ConnectionsActivityPresenter presenter;
 
@@ -86,6 +86,8 @@ public class ConnectionsActivity extends AppCompatActivity implements
     @Override
     public void displayConnections(List<ConnectidConnection> connections) {
         Log.i("MVP view", "displayConnections received " + connections.size() + " connections");
+        emptyListTv.setVisibility(View.INVISIBLE);
+        recyclerView.setVisibility(View.VISIBLE);
 
         adapter.setConnections(connections);
     }
@@ -116,11 +118,21 @@ public class ConnectionsActivity extends AppCompatActivity implements
     @Override
     public void displayNoConnections() {
         Log.i("MVP view", "displayNoConnections received empty list");
+
+        // TODO Make pretty strings sometime.
+        emptyListTv.setText("Need to remember someone?\nClick the button to get started!");
+        recyclerView.setVisibility(View.INVISIBLE);
+        emptyListTv.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void displayError() {
         Log.i("MVP view", "displayError called due to error");
+
+        // TODO Make pretty strings sometime.
+        emptyListTv.setText("Fatal error.\nProbably a database failure.");
+        recyclerView.setVisibility(View.INVISIBLE);
+        emptyListTv.setVisibility(View.VISIBLE);
     }
 
     @Override
