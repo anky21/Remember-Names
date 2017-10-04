d7efa34a1d191e294b7bfd7be5c62db2070992ea
Anky An <anky25@gmail.com>
2017-10-05 07:09:41 +1000

    feat: Show all tags on the screen

--- app/src/main/java/me/anky/connectid/editTag/EditTagActivity.java
+++ app/src/main/java/me/anky/connectid/editTag/EditTagActivity.java
@@ -3,12 +3,17 @@ package me.anky.connectid.editTag;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import me.anky.connectid.R;
@@ -17,6 +22,12 @@ import me.anky.connectid.edit.EditActivity;
 import me.anky.connectid.root.ConnectidApplication;
 
 public class EditTagActivity extends AppCompatActivity implements EditTagActivityMVP.View {
+
+    public List<ConnectionTag> allTags = new ArrayList<>();
+
+    @BindView(R.id.all_tags)
+    LinearLayout allTagsLinear;
+
 
     @Inject
     EditTagActivityPresenter presenter;
@@ -37,16 +48,27 @@ public class EditTagActivity extends AppCompatActivity implements EditTagActivit
     protected void onResume() {
         super.onResume();
         presenter.setView(this);
+        presenter.loadTags();
     }
 
     @Override
     public void displayAllTags(List<ConnectionTag> allTags) {
-
+        for (ConnectionTag tag:allTags){
+            Log.v("testing", "tag is " + tag.getTag());
+            TextView tagTv = new TextView(this);
+            tagTv.setText(tag.getTag());
+            tagTv.setTextSize(14);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8,2,8,2);
+            tagTv.setLayoutParams(params);
+            tagTv.setBackgroundResource(R.drawable.round_bg_blue);
+            allTagsLinear.addView(tagTv);
+        }
     }
 
     @Override
     public void displayNoTags() {
-
     }
 
     @Override
--- app/src/main/java/me/anky/connectid/editTag/EditTagActivityPresenter.java
+++ app/src/main/java/me/anky/connectid/editTag/EditTagActivityPresenter.java
@@ -1,8 +1,15 @@
 package me.anky.connectid.editTag;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+import me.anky.connectid.data.ConnectionTag;
 import me.anky.connectid.data.ConnectionsDataSource;
 
 /**
@@ -28,11 +35,29 @@ public class EditTagActivityPresenter implements EditTagActivityMVP.Presenter {
 
     @Override
     public void loadTags() {
+        DisposableSingleObserver<List<ConnectionTag>> disposableSingleObserver =
+                dataSource.getTags().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<ConnectionTag>>() {
+                    @Override
+                    public void onSuccess(List<ConnectionTag> connectionTags) {
+                        if (connectionTags.isEmpty()){
+                            view.displayNoTags();
+                        }else {
+                            view.displayAllTags(connectionTags);
+                        }
+                    }
 
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                });
+        compositeDisposable.add(disposableSingleObserver);
     }
 
     @Override
     public void unsubscribe() {
-
+        compositeDisposable.clear();
     }
 }
