823e65234b849e6cfb6f056308fc1d2a5d03b9c9
Anky <anky_au@hotmail.com>
2017-11-05 03:39:53 +1000

    feat: Load all tags to TagsActivity

--- app/src/main/java/me/anky/connectid/tags/TagsActivity.java
+++ app/src/main/java/me/anky/connectid/tags/TagsActivity.java
@@ -2,27 +2,51 @@ package me.anky.connectid.tags;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import me.anky.connectid.R;
 import me.anky.connectid.data.ConnectionTag;
+import me.anky.connectid.root.ConnectidApplication;
 
 public class TagsActivity extends AppCompatActivity implements TagsActivityMVP.View{
+
+    @BindView(R.id.all_tags_recyclerview)
+    RecyclerView recyclerView;
+
+    @Inject
+    TagsActivityPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tags);
+        ButterKnife.bind(this);
+
+        ((ConnectidApplication) getApplication()).getApplicationComponent().inject(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.setView(this);
+        presenter.loadTags();
     }
 
     @Override
     public void displayTags(List<ConnectionTag> allTags) {
-
+        Toast.makeText(this, "all tags", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void displayNoTags() {
-
+        Toast.makeText(this, "no tags", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/me/anky/connectid/tags/TagsActivityPresenter.java
+++ app/src/main/java/me/anky/connectid/tags/TagsActivityPresenter.java
@@ -1,8 +1,14 @@
 package me.anky.connectid.tags;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+import me.anky.connectid.data.ConnectionTag;
 import me.anky.connectid.data.ConnectionsDataSource;
 
 /**
@@ -26,7 +32,25 @@ public class TagsActivityPresenter implements TagsActivityMVP.Presenter {
     }
 
     public void loadTags() {
+        DisposableSingleObserver<List<ConnectionTag>> disposableSingleObserver =
+                dataSource.getTags().subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<List<ConnectionTag>>() {
+                            @Override
+                            public void onSuccess(List<ConnectionTag> connectionTags) {
+                                if (connectionTags.isEmpty()) {
+                                    view.displayNoTags();
+                                } else {
+                                    view.displayTags(connectionTags);
+                                }
+                            }
 
+                            @Override
+                            public void onError(Throwable e) {
+
+                            }
+                        });
+        compositeDisposable.add(disposableSingleObserver);
     }
 
     @Override
